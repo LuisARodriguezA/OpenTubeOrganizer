fdf572a4e94291b64fa162a1cfd6646224b2fd95
LuigiToby <luirodriguezar@unal.edu.co>
2023-11-28 15:10:30 -0500

    Creación canales.csv

--- beta.py
+++ beta.py
@@ -1,4 +1,14 @@
+import pandas as pd
 
-df = pd.DataFrame(columns=['Nombre del canal', 'Categoría', 'Enlace del último video'])
+# Intentar cargar el DataFrame desde el archivo existente
+try:
+    df = pd.read_csv('canales.csv', index_col='Nombre del canal')
+except FileNotFoundError:
+    # Si el archivo no existe, crear un DataFrame vacío
+    df = pd.DataFrame(columns=['Nombre del canal', 'Categoría', 'Enlace del último video'])
+    df.set_index('Nombre del canal', inplace=True)
 
-print(df)
+# ... Resto de tu código ...
+
+# Guardar el DataFrame al final del programa o cuando se realicen cambios
+df.to_csv('canales.csv')
