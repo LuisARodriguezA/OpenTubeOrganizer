7e8beabaf11cc94c1b78f6ffda4ac9c818c17293
LuigiToby <luirodriguezar@unal.edu.co>
2023-11-28 17:01:29 -0500

    work on main.py feed function

--- main.py
+++ main.py
@@ -1,44 +1,41 @@
-# Imports
+# librerias
 # --------------------------------------------------------------------------------------
 import pandas as pd
 from tabulate import tabulate
 # --------------------------------------------------------------------------------------
-
-
-
-lista_indice = ["a","b","c","d","e"] # Creación de indices para el menú de la app
-
+# assets
+from assets import lista_indice_menu
+from assets import menu
+# --------------------------------------------------------------------------------------
 def main():
     setup() # Hacemos un setup inicial para asegurarnos que el dataframe este bien creado
     menu() # Empezamos con el menú o "interfaz de usuario"
-
+# --------------------------------------------------------------------------------------
 def menu():
     
     # Mensage de bienvenida
-    print("Bienvenido a OpenTubeOrganizer, ¿Que deseas hacer?\n a. Ver lista de subscripciones\n b. Agregar/Borrar canal\n c. Agregar/Borrar categoría\n d. Borrar información\n e. Salir")
+    print(menu)
     
     # Chekeamos que el input del usuario sea valido antes de pasarlo
     while True:
         entrada = input("Ingresa el indice para seleccionar una opción: ")
-        if entrada not in lista_indice: 
+        if entrada not in lista_indice_menu: 
             print("Entrada no valida, vuelve a intentarlo")
             continue
         break
     
-    print("Entrada valida")
-
     # Redirección a la función requerida
-    if entrada == lista_indice[0]:
+    if entrada == lista_indice_menu[0]:
         feed()
-    elif entrada == lista_indice[1]:
+    elif entrada == lista_indice_menu[1]:
         canales()
-    elif entrada == lista_indice[2]:
+    elif entrada == lista_indice_menu[2]:
         categorias()
-    elif entrada == lista_indice[3]:
+    elif entrada == lista_indice_menu[3]:
         borrar()
-    elif entrada == lista_indice[4]:
+    elif entrada == lista_indice_menu[4]:
         exit()
-
+# --------------------------------------------------------------------------------------
 def setup():
     # Intentar cargar el DataFrame desde el archivo existente
     try:
@@ -51,18 +48,39 @@ def setup():
         df.index.names = ['Indices']
         # df.columns.names= ['group']
         df.to_csv('canales.csv') # Guardar el DataFrame al final del programa o cuando se realicen cambios
-
+# --------------------------------------------------------------------------------------
 def feed():
     df = pd.read_csv('canales.csv')
     print(tabulate(df, headers = 'keys', tablefmt = 'psql'))
+    print("Bienvenido a OpenTubeOrganizer, ¿Que deseas hacer?\n a. Ver lista de subscripciones\n b. Agregar/Borrar canal\n c. Agregar/Borrar categoría\n d. Borrar información\n e. Salir")
 
+    while True:
+        entrada = input("Ingresa el indice para seleccionar una opción: ")
+        if entrada not in lista_indice_menu: 
+            print("Entrada no valida, vuelve a intentarlo")
+            continue
+        break
+    
+    # Redirección a la función requerida
+    if entrada == lista_indice_menu[0]:
+        feed()
+    elif entrada == lista_indice_menu[1]:
+        canales()
+    elif entrada == lista_indice_menu[2]:
+        categorias()
+    elif entrada == lista_indice_menu[3]:
+        borrar()
+    elif entrada == lista_indice_menu[4]:
+        exit()
+# --------------------------------------------------------------------------------------
 def canales():
     print("N/A")
-
+# --------------------------------------------------------------------------------------
 def categorias():
     print("N/A")
-
+# --------------------------------------------------------------------------------------
 def borrar():
     print("N/A")
-
+# --------------------------------------------------------------------------------------
 main()
+# --------------------------------------------------------------------------------------
