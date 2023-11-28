7acd79b132c001c30e5b9e695d918dd45c02ac06
LuigiToby <luirodriguezar@unal.edu.co>
2023-11-28 15:25:17 -0500

    Upd dependencies

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-lista_indice = ["a","b","c","d","e"]
-initial_setup = 0
+# Imports
+# --------------------------------------------------------------------------------------
+import pandas as pd
+# --------------------------------------------------------------------------------------
+
+
+
+lista_indice = ["a","b","c","d","e"] # Creación de indices para el menú de la app
+
 def main():
-    setup()
-    menu()
+    setup() # Hacemos un setup inicial para asegurarnos que el dataframe este bien creado
+    menu() # Empezamos con el menú o "interfaz de usuario"
 
 def menu():
     
+    # Mensage de bienvenida
     print("Bienvenido a OpenTubeOrganizer, ¿Que deseas hacer?\n a. Ver lista de subscripciones\n b. Agregar/Borrar canal\n c. Agregar/Borrar categoría\n d. Borrar información\n e. Salir")
+    
+    # Chekeamos que el input del usuario sea valido antes de pasarlo
     while True:
         entrada = input("Ingresa el indice para seleccionar una opción: ")
         if entrada not in lista_indice: 
@@ -16,6 +26,7 @@ def menu():
     
     print("Entrada valida")
 
+    # Redirección a la función requerida
     if entrada == lista_indice[0]:
         feed()
     elif entrada == lista_indice[1]:
